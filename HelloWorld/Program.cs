2b3ee730285ab33732a46ba82ff3465f1df68dd8
premtodkar <premkumart@cybage.com>
2017-03-08 18:21:26 +0530

    Update Program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!!!");
-            Console.WriteLine("Welcome to Earth Planet");
+            Console.WriteLine("Welcome to Planet Earth");
         }
     }
 }
